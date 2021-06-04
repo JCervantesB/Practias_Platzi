8427103c464a68960e97ece11c444ddd30d448ef
JCervantesB <65304872+JCervantesB@users.noreply.github.com>
2021-06-04 14:44:12 -0600

    Update practia1.js

--- practia1.js
+++ practia1.js
@@ -1,5 +1,10 @@
- // Juego piedra, papel o tijeras.
- 
+// Julio Cervantes, Curso JavaScript Platzi
+// Este es mi primer reto de comprobaciones con Switch.
+// Este es un pequeño juego del "Piedra, Papel o Tijera", donde el navegador te solicita elejir de las 3 opciones
+// Se emplea una variable para "cpu" donde mediante la función Math.random() https://www.w3schools.com/js/js_random.asp elige una de las 3 posibles opciones
+// El sistema compara tu opcion, con la de la cpu y determina un ganador.
+
+
 var pregunta = prompt("Ingresa tu opción: piedra, papel o tijera "); 
 var usuario = pregunta.toLowerCase();
 var opciones = ["piedra", "papel", "tijera"];
